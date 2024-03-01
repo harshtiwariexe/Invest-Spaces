532acb0e34c38d1bb52d4e4584ffd499a97221d0
Harsh Tiwari <harsh.official1802@gmail.com>
2024-03-02 01:41:39 +0530

    Connectig to database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connection Succesful");
+}).catch(err=>{
+    console.log(err);
+})
+
 
 const app = express();
 
-app.listen('3000',()=>{
-    console.log("Server running on port 3000");
-})
+app.listen('3000',()=>{console.log("Server running on port 3000")})
